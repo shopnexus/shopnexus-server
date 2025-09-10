0d0a3061895f235826ff8f389927477c694af51f
Đậu Khoa <khoakomlem@gmail.com>
2025-09-10 19:18:18 +0700

    workaround for scanning custom type on pgx/v5

--- internal/client/pgxpool/pgxpool.go
+++ internal/client/pgxpool/pgxpool.go
@@ -6,7 +6,9 @@ import (
 
 	"shopnexus-remastered/internal/logger"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -35,7 +37,59 @@ func New(opts Options) (*pgxpool.Pool, error) {
 		logger.Log.Warn("PostgreSQL notice: " + notice.Message)
 	}
 
+	pool, err := pgxpool.NewWithConfig(context.Background(), connConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	// Collect the custom data types once, store them in memory, and register them for every future connection.
+	customTypes, err := getCustomDataTypes(context.Background(), pool)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(customTypes)
+	connConfig.AfterConnect = func(ctx context.Context, conn *pgx.Conn) error {
+		for _, t := range customTypes {
+			conn.TypeMap().RegisterType(t)
+		}
+		return nil
+	}
+
+	// Immediately close the old pool and open a new one with the new config.
+	pool.Close()
 	return pgxpool.NewWithConfig(context.Background(), connConfig)
+}
+
+// Any custom DB types made with CREATE TYPE need to be registered with pgx.
+// https://github.com/kyleconroy/sqlc/issues/2116
+// https://stackoverflow.com/questions/75658429/need-to-update-psql-row-of-a-composite-type-in-golang-with-jack-pgx
+// https://pkg.go.dev/github.com/jackc/pgx/v5/pgtype
+func getCustomDataTypes(ctx context.Context, pool *pgxpool.Pool) ([]*pgtype.Type, error) {
+	// Get a single connection just to load type information.
+	conn, err := pool.Acquire(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Release()
+
+	dataTypeNames := []string{
+		"shared.resource_type",
+		"shared._resource_type",
+		"inventory.stock_type",
+		"inventory._stock_type",
+	}
+
+	var typesToRegister []*pgtype.Type
+	for _, typeName := range dataTypeNames {
+		dataType, err := conn.Conn().LoadType(ctx, typeName)
+		if err != nil {
+			return nil, err
+		}
+		// You need to register only for this connection too, otherwise the array type will look for the register element type.
+		conn.Conn().TypeMap().RegisterType(dataType)
+		typesToRegister = append(typesToRegister, dataType)
+	}
+	return typesToRegister, nil
 }
 
 func GetConnStr(opts Options) string {
