672c311154db1390e5bdf872fde7ef518b72e3c6
Đậu Khoa <khoakomlem@gmail.com>
2025-09-10 20:08:05 +0700

    add some slice utils

--- internal/utils/slice/slice.go
+++ internal/utils/slice/slice.go
@@ -32,3 +32,59 @@ func Map[T, U any](slice []T, transform func(T) U) []U {
 	}
 	return result
 }
+
+func Filter[T any](slice []T, predicate func(T) bool) []T {
+	result := make([]T, 0)
+	for _, v := range slice {
+		if predicate(v) {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+func FilterMap[T any, U any](slice []T, transform func(T) (U, bool)) []U {
+	result := make([]U, 0)
+	for _, v := range slice {
+		if mapped, ok := transform(v); ok {
+			result = append(result, mapped)
+		}
+	}
+	return result
+}
+
+func NewMap[T any, G comparable](items []T, keyFunc func(T) G) map[G]*T {
+	m := make(map[G]*T)
+	for _, item := range items {
+		k := keyFunc(item)
+		m[k] = &item
+	}
+	return m
+}
+
+type SliceMapID[T any, G comparable] struct {
+	Map map[G]*T
+	IDs []G
+}
+
+func NewSliceMapID[T any, G comparable](items []T, keyFunc func(T) G) *SliceMapID[T, G] {
+	m := make(map[G]*T)
+	ids := make([]G, 0, len(items))
+	for _, item := range items {
+		k := keyFunc(item)
+		m[k] = &item
+		ids = append(ids, k)
+	}
+	return &SliceMapID[T, G]{
+		Map: m,
+		IDs: ids,
+	}
+}
+
+func MapToSlice[T any, G comparable](m map[G]T) []T {
+	result := make([]T, 0, len(m))
+	for _, v := range m {
+		result = append(result, v)
+	}
+	return result
+}
