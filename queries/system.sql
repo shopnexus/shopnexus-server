148b7c241098aafd29b724471d0f559395446695
Đậu Khoa <khoakomlem@gmail.com>
2025-09-30 23:50:42 +0700

    order by date_updated

--- queries/system.sql
+++ queries/system.sql
@@ -2,7 +2,7 @@
 SELECT id, ref_id, ref_type
 FROM system.search_sync
 WHERE (is_stale_metadata = sqlc.narg('is_stale_metadata') OR is_stale_embedding = sqlc.narg('is_stale_embedding')) AND ref_type = $1
-ORDER BY date_created ASC
+ORDER BY date_updated ASC
 FOR UPDATE SKIP LOCKED
 LIMIT sqlc.arg('limit');
 
