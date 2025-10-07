cd60a3f6b84929961c16e7901202eb25db2ca309
Đậu Khoa <khoakomlem@gmail.com>
2025-10-07 21:22:24 +0700

    add service option

---
+++ internal/module/shared/biz/option.go
@@ -0,0 +1,62 @@
+package sharedbiz
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"shopnexus-remastered/internal/db"
+	sharedmodel "shopnexus-remastered/internal/module/shared/model"
+	"shopnexus-remastered/internal/utils/pgutil"
+)
+
+func (b *SharedBiz) UpdateServiceOptions(ctx context.Context, category string, configs []sharedmodel.OptionConfig) error {
+	txStorage, err := b.storage.BeginTx(ctx)
+	if err != nil {
+		return err
+	}
+	defer txStorage.Rollback(ctx)
+
+	for _, cfg := range configs {
+		_, err := txStorage.GetSharedServiceOption(ctx, pgutil.StringToPgText(cfg.ID))
+		if err != nil {
+			// if not found, create it
+			if errors.Is(err, sql.ErrNoRows) {
+				_, err = txStorage.CreateSharedServiceOption(ctx, db.CreateSharedServiceOptionParams{
+					ID:          cfg.ID,
+					Category:    category,
+					Name:        cfg.Name,
+					Description: cfg.Description,
+					Provider:    cfg.Provider,
+					Method:      string(cfg.Method),
+					IsActive:    cfg.IsActive,
+				})
+				if err != nil {
+					return err
+				}
+			}
+
+			// other db error
+			return err
+		} else {
+			// update existing
+			_, err = txStorage.UpdateSharedServiceOption(ctx, db.UpdateSharedServiceOptionParams{
+				ID:          cfg.ID,
+				Name:        pgutil.StringToPgText(cfg.Name),
+				Description: pgutil.StringToPgText(cfg.Description),
+				Provider:    pgutil.StringToPgText(cfg.Provider),
+				Method:      pgutil.StringToPgText(string(cfg.Method)),
+				IsActive:    pgutil.BoolToPgBool(cfg.IsActive),
+				Category:    pgutil.StringToPgText(category),
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if err := txStorage.Commit(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ internal/module/shared/model/option.go
@@ -0,0 +1,12 @@
+package sharedmodel
+
+type OptionMethod string
+
+type OptionConfig struct {
+	ID          string       // e.g. "ghn-standard", "ghtk-express", "vnpay-qr", "cod"
+	Provider    string       // "ghn", "ghtk", "vnpost", "vnpay", "momo"
+	Method      OptionMethod // "standard", "express" "qr", "bank", "atm", "cod"
+	Name        string       // e.g. "Giao hàng nhanh - Standard", "Giao hàng tiết kiệm - Express", "VNPay - QR", "COD"
+	Description string
+	IsActive    bool
+}
