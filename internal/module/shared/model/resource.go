c782232f5dc6246c6adfb8d836a03fc00e5037a9
Đậu Khoa <khoakomlem@gmail.com>
2025-10-08 23:36:46 +0700

    update resource model

--- internal/module/shared/model/resource.go
+++ internal/module/shared/model/resource.go
@@ -5,16 +5,16 @@ import (
 )
 
 type CreateResource struct {
-	Order int32  `json:"order" validate:"required,gte=0"`
-	Url   string `json:"url" validate:"required,url"`
+	FileID int64 `json:"file_id" validate:"required"`
 }
 
 type Resource struct {
-	ID       int64      `json:"id"`
-	Mime     string     `json:"mime"`
-	Url      string     `json:"url"`
-	FileSize null.Int64 `json:"file_size"`
-	Width    null.Int32 `json:"width"`
-	Height   null.Int32 `json:"height"`
-	Duration null.Float `json:"duration"`
+	ID       int64       `json:"id"`
+	Url      string      `json:"url"`
+	Mime     string      `json:"mime"`
+	FileSize null.Int64  `json:"file_size"`
+	Width    null.Int32  `json:"width"`
+	Height   null.Int32  `json:"height"`
+	Duration null.Float  `json:"duration"`
+	Checksum null.String `json:"checksum"`
 }
