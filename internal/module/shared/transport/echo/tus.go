15f154a2ed2ac65cc8348f8bfc629055524dec44
Đậu Khoa <khoakomlem@gmail.com>
2025-10-08 23:36:59 +0700

    add get file

---
+++ internal/module/shared/transport/echo/tus.go
@@ -0,0 +1,38 @@
+package sharedecho
+
+import (
+	"fmt"
+	"net/http"
+	"shopnexus-remastered/config"
+	"shopnexus-remastered/internal/module/shared/transport/echo/response"
+
+	"github.com/labstack/echo/v4"
+)
+
+type GetFileRequest struct {
+	ResourceCode string `param:"resource_code" validate:"required,uuid"`
+}
+
+func (h *Handler) GetFile(c echo.Context) error {
+	var req GetFileRequest
+	if err := c.Bind(&req); err != nil {
+		return response.FromError(c.Response().Writer, http.StatusBadRequest, err)
+	}
+	if err := c.Validate(&req); err != nil {
+		return response.FromError(c.Response().Writer, http.StatusBadRequest, err)
+	}
+
+	if config.GetConfig().Filestore.Type == "local" {
+		// Manually serve the file using tus handler (because we disabled download in tus config before)
+		h.tusHandler.GetFile(c.Response().Writer, c.Request())
+		return nil
+	}
+	if config.GetConfig().Filestore.Type == "s3" {
+		// Return redirect to cloudfront URL
+
+		url := fmt.Sprintf("https://%s/%s", config.GetConfig().Filestore.S3.CloudfrontURL, req.ResourceCode)
+		return c.Redirect(http.StatusSeeOther, url)
+	}
+
+	return response.FromError(c.Response().Writer, http.StatusInternalServerError, fmt.Errorf("unsupported filestore type"))
+}
