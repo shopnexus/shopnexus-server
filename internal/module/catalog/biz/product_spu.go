93e673684fadb76f31ffce600323d706bac4481a
Đậu Khoa <khoakomlem@gmail.com>
2025-10-14 17:59:06 +0700

    update product spu response

--- internal/module/catalog/biz/product_spu.go
+++ internal/module/catalog/biz/product_spu.go
@@ -10,33 +10,58 @@ import (
 
 	"shopnexus-remastered/internal/db"
 	authmodel "shopnexus-remastered/internal/module/auth/model"
+	catalogmodel "shopnexus-remastered/internal/module/catalog/model"
 	searchmodel "shopnexus-remastered/internal/module/search/model"
+	sharedbiz "shopnexus-remastered/internal/module/shared/biz"
 	sharedmodel "shopnexus-remastered/internal/module/shared/model"
 	"shopnexus-remastered/internal/module/shared/transport/echo/validator"
 	"shopnexus-remastered/internal/utils/pgutil"
+	"shopnexus-remastered/internal/utils/slice"
 
 	"github.com/guregu/null/v6"
 )
 
+func (b *CatalogBiz) getCategoryName(ctx context.Context, categoryID int64) string {
+	category, err := b.storage.GetCatalogCategory(ctx, db.GetCatalogCategoryParams{
+		ID: pgutil.Int64ToPgInt8(categoryID),
+	})
+	if err != nil {
+		return ""
+	}
+	return category.Name
+}
+
+func (b *CatalogBiz) getBrandName(ctx context.Context, brandID int64) string {
+	brand, err := b.storage.GetCatalogBrand(ctx, db.GetCatalogBrandParams{
+		ID: pgutil.Int64ToPgInt8(brandID),
+	})
+	if err != nil {
+		return ""
+	}
+	return brand.Name
+}
+
 type ListProductSpuParams struct {
 	sharedmodel.PaginationParams
+	Account    authmodel.AuthenticatedAccount
+	ID         []int64  `validate:"omitempty,dive,gt=0"`
 	Code       []string `validate:"omitempty,dive,min=1,max=100"`
-	AccountID  []int64  `validate:"omitempty,dive,gt=0"`
 	CategoryID []int64  `validate:"omitempty,dive,gt=0"`
 	BrandID    []int64  `validate:"omitempty,dive,gt=0"`
 	IsActive   []bool   `validate:"omitempty,dive"`
 }
 
-func (b *CatalogBiz) ListProductSpu(ctx context.Context, params ListProductSpuParams) (sharedmodel.PaginateResult[db.CatalogProductSpu], error) {
-	var zero sharedmodel.PaginateResult[db.CatalogProductSpu]
+func (b *CatalogBiz) ListProductSpu(ctx context.Context, params ListProductSpuParams) (sharedmodel.PaginateResult[catalogmodel.ProductSpu], error) {
+	var zero sharedmodel.PaginateResult[catalogmodel.ProductSpu]
 
 	if err := validator.Validate(params); err != nil {
 		return zero, err
 	}
 
 	total, err := b.storage.CountCatalogProductSpu(ctx, db.CountCatalogProductSpuParams{
-		Code:       params.Code,
-		AccountID:  params.AccountID,
+		ID:   params.ID,
+		Code: params.Code,
+		// AccountID:  []int64{params.Account.ID},
 		CategoryID: params.CategoryID,
 		BrandID:    params.BrandID,
 		IsActive:   params.IsActive,
@@ -45,11 +70,12 @@ func (b *CatalogBiz) ListProductSpu(ctx context.Context, params ListProductSpuPa
 		return zero, err
 	}
 
-	spus, err := b.storage.ListCatalogProductSpu(ctx, db.ListCatalogProductSpuParams{
-		Limit:      pgutil.Int32ToPgInt4(params.GetLimit()),
-		Offset:     pgutil.Int32ToPgInt4(params.Offset()),
-		Code:       params.Code,
-		AccountID:  params.AccountID,
+	dbSpus, err := b.storage.ListCatalogProductSpu(ctx, db.ListCatalogProductSpuParams{
+		Limit:  pgutil.Int32ToPgInt4(params.GetLimit()),
+		Offset: pgutil.Int32ToPgInt4(params.Offset()),
+		ID:     params.ID,
+		Code:   params.Code,
+		// AccountID:  []int64{params.Account.ID}, // TODO: uncomment this
 		CategoryID: params.CategoryID,
 		BrandID:    params.BrandID,
 		IsActive:   params.IsActive,
@@ -58,7 +84,67 @@ func (b *CatalogBiz) ListProductSpu(ctx context.Context, params ListProductSpuPa
 		return zero, err
 	}
 
-	return sharedmodel.PaginateResult[db.CatalogProductSpu]{
+	spuIDs := slice.Map(dbSpus, func(spu db.CatalogProductSpu) int64 { return spu.ID })
+
+	tagRefs, err := b.storage.ListCatalogProductSpuTag(ctx, db.ListCatalogProductSpuTagParams{
+		SpuID: spuIDs,
+	})
+	if err != nil {
+		return zero, err
+	}
+	tags, err := b.storage.ListCatalogTag(ctx, db.ListCatalogTagParams{
+		ID: slice.Map(tagRefs, func(ref db.CatalogProductSpuTag) int64 { return ref.TagID }),
+	})
+	if err != nil {
+		return zero, err
+	}
+	tagsMap := slice.GroupBySlice(tags, func(tag db.CatalogTag) (int64, string) { return tag.ID, tag.Tag })
+
+	// Get first image of the product
+	resources, err := b.storage.ListSortedResources(ctx, db.ListSortedResourcesParams{
+		RefType: db.SharedResourceRefTypeProductSpu,
+		RefID:   spuIDs,
+	})
+	if err != nil {
+		return zero, err
+	}
+	resourcesMap := slice.GroupBySlice(resources, func(r db.ListSortedResourcesRow) (int64, db.ListSortedResourcesRow) { return r.RefID, r })
+
+	// Calculate rating score
+	ratings, err := b.storage.ListRating(ctx, db.ListRatingParams{
+		RefType: db.CatalogCommentRefTypeProductSpu,
+		RefID:   spuIDs,
+	})
+	if err != nil {
+		return zero, err
+	}
+	ratingMap := slice.GroupBy(ratings, func(r db.ListRatingRow) (int64, db.ListRatingRow) { return r.RefID, r })
+
+	var spus []catalogmodel.ProductSpu
+	for _, spu := range dbSpus {
+		spus = append(spus, catalogmodel.ProductSpu{
+			ID:            spu.ID,
+			Code:          spu.Code,
+			Category:      b.getCategoryName(ctx, spu.CategoryID),
+			Brand:         b.getBrandName(ctx, spu.BrandID),
+			FeaturedSkuID: null.NewInt(spu.FeaturedSkuID.Int64, spu.FeaturedSkuID.Valid),
+			Name:          spu.Name,
+			Description:   spu.Description,
+			IsActive:      spu.IsActive,
+			DateCreated:   spu.DateCreated.Time,
+			DateUpdated:   spu.DateUpdated.Time,
+			Rating: catalogmodel.ProductRating{
+				Score: ratingMap[spu.ID].Score,
+				Total: ratingMap[spu.ID].Count,
+			},
+			Tags: slice.NonNil(tagsMap[spu.ID]),
+			Resources: slice.Map(resourcesMap[spu.ID], func(r db.ListSortedResourcesRow) string {
+				return sharedbiz.GetResourceURL(string(r.Provider), r.ObjectKey)
+			}),
+		})
+	}
+
+	return sharedmodel.PaginateResult[catalogmodel.ProductSpu]{
 		PageParams: params.PaginationParams,
 		Total:      null.IntFrom(total),
 		Data:       spus,
@@ -87,7 +173,7 @@ func (b *CatalogBiz) CreateProductSpu(ctx context.Context, params CreateProductS
 	defer txStorage.Rollback(ctx)
 
 	spu, err := txStorage.CreateDefaultCatalogProductSpu(ctx, db.CreateDefaultCatalogProductSpuParams{
-		Code:        "generate slug here", // TODO: create function generate slug
+		Code:        GenerateSlug(params.Name),
 		AccountID:   params.Account.ID,
 		CategoryID:  params.CategoryID,
 		BrandID:     params.BrandID,
@@ -134,8 +220,7 @@ func (b *CatalogBiz) UpdateProductSpu(ctx context.Context, params UpdateProductS
 
 	var code null.String
 	if params.Name.Valid {
-		c := fmt.Sprintf("%s.%s", slug.Make(params.Name.String), uuid.NewString())
-		code.SetValid(c)
+		code.SetValid(GenerateSlug(params.Name.String))
 	}
 
 	// Update the product spu
@@ -205,4 +290,8 @@ func (b *CatalogBiz) DeleteProductSpu(ctx context.Context, params DeleteProductS
 	}
 
 	return nil
+}
+
+func GenerateSlug(name string) string {
+	return fmt.Sprintf("%s.%s", slug.Make(name), uuid.NewString())
 }
