6f6caa6c7023aa3ab3b7ce5256c19c8456d3df7e
Đậu Khoa <khoakomlem@gmail.com>
2025-10-14 17:58:11 +0700

    add null bool to slice

--- internal/utils/pgutil/slice.go
+++ internal/utils/pgutil/slice.go
@@ -1,5 +1,7 @@
 package pgutil
 
+import "github.com/guregu/null/v6"
+
 // SliceToPgArray converts a slice of values to a pgtype array
 func SliceToPgArray[T any, P any](slice []T, converter func(T) P) []P {
 	result := make([]P, len(slice))
@@ -7,4 +9,12 @@ func SliceToPgArray[T any, P any](slice []T, converter func(T) P) []P {
 		result[i] = converter(item)
 	}
 	return result
+}
+
+// NullBoolToSlice converts a null.Bool to a slice of bool
+func NullBoolToSlice(b null.Bool) []bool {
+	if b.Valid {
+		return []bool{b.Bool}
+	}
+	return nil
 }
