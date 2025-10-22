412cc9c276743a440d095597a70cd17cb50ddf59
Đậu Khoa <khoakomlem@gmail.com>
2025-10-23 00:37:57 +0700

    add tag apis

---
+++ internal/module/catalog/biz/tag.go
@@ -0,0 +1,64 @@
+package catalogbiz
+
+import (
+	"context"
+
+	"shopnexus-remastered/internal/db"
+	authmodel "shopnexus-remastered/internal/module/auth/model"
+	sharedmodel "shopnexus-remastered/internal/module/shared/model"
+	"shopnexus-remastered/internal/module/shared/transport/echo/validator"
+	"shopnexus-remastered/internal/utils/pgutil"
+
+	"github.com/guregu/null/v6"
+)
+
+type ListTagParams struct {
+	sharedmodel.PaginationParams
+}
+
+func (b *CatalogBiz) ListTag(ctx context.Context, params ListTagParams) (sharedmodel.PaginateResult[db.CatalogTag], error) {
+	var zero sharedmodel.PaginateResult[db.CatalogTag]
+
+	if err := validator.Validate(params); err != nil {
+		return zero, err
+	}
+
+	total, err := b.storage.CountCatalogTag(ctx, nil)
+	if err != nil {
+		return zero, err
+	}
+
+	dbTags, err := b.storage.ListCatalogTag(ctx, db.ListCatalogTagParams{
+		Limit:  pgutil.Int32ToPgInt4(params.GetLimit()),
+		Offset: pgutil.Int32ToPgInt4(params.Offset()),
+	})
+	if err != nil {
+		return zero, err
+	}
+
+	return sharedmodel.PaginateResult[db.CatalogTag]{
+		PageParams: params.PaginationParams,
+		Total:      null.IntFrom(total),
+		Data:       dbTags,
+	}, nil
+}
+
+type GetTagParams struct {
+	Account authmodel.AuthenticatedAccount
+	Tag     string `validate:"required,min=1,max=100"`
+}
+
+func (b *CatalogBiz) GetTag(ctx context.Context, params GetTagParams) (db.CatalogTag, error) {
+	var zero db.CatalogTag
+
+	if err := validator.Validate(params); err != nil {
+		return zero, err
+	}
+
+	tag, err := b.storage.GetCatalogTag(ctx, pgutil.StringToPgText(params.Tag))
+	if err != nil {
+		return zero, err
+	}
+
+	return tag, nil
+}
