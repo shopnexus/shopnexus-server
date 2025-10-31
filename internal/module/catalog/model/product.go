ed6de06a794bff788f652c662114788581f04d85
Đậu Khoa <khoakomlem@gmail.com>
2025-10-31 10:36:59 +0700

    update product model

--- internal/module/catalog/model/product.go
+++ internal/module/catalog/model/product.go
@@ -1,26 +1,28 @@
 package catalogmodel
 
 import (
+	"shopnexus-remastered/internal/db"
+	sharedmodel "shopnexus-remastered/internal/module/shared/model"
 	"time"
 
 	"github.com/guregu/null/v6"
 )
 
 type ProductSpu struct {
-	ID            int64      `json:"id"`
-	Code          string     `json:"code"`
-	Category      string     `json:"category"`
-	Brand         string     `json:"brand"`
-	FeaturedSkuID null.Int64 `json:"featured_sku_id"`
-	Name          string     `json:"name"`
-	Description   string     `json:"description"`
-	IsActive      bool       `json:"is_active"`
-	DateCreated   time.Time  `json:"date_created"`
-	DateUpdated   time.Time  `json:"date_updated"`
+	ID            int64              `json:"id"`
+	Code          string             `json:"code"`
+	Category      db.CatalogCategory `json:"category"`
+	Brand         db.CatalogBrand    `json:"brand"`
+	FeaturedSkuID null.Int64         `json:"featured_sku_id"`
+	Name          string             `json:"name"`
+	Description   string             `json:"description"`
+	IsActive      bool               `json:"is_active"`
+	DateCreated   time.Time          `json:"date_created"`
+	DateUpdated   time.Time          `json:"date_updated"`
 
-	Rating    ProductRating `json:"rating"`
-	Tags      []string      `json:"tags"`
-	Resources []string      `json:"resources"`
+	Rating    ProductRating          `json:"rating"`
+	Tags      []string               `json:"tags"`
+	Resources []sharedmodel.Resource `json:"resources"`
 }
 
 type ProductSku struct {
