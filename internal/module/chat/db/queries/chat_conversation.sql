462c9ec957f1b938bffd357b0439c528eeff97c9
Khoa Bean <khoakomlem@gmail.com>
2026-03-23 13:00:08 +0700

    update chat queries

--- internal/module/chat/db/queries/chat_conversation.sql
+++ internal/module/chat/db/queries/chat_conversation.sql
@@ -1,32 +1,101 @@
--- name: GetConversation :one
+-- name: GetChatConversation :one
 SELECT *
 FROM "chat"."conversation"
-WHERE "id" = sqlc.arg('id');
+WHERE ("id" = sqlc.narg('id')) OR ("customer_id" = sqlc.narg('customer_id') AND "vendor_id" = sqlc.narg('vendor_id'));
 
--- name: GetConversationByParticipants :one
-SELECT *
+-- name: CountChatConversation :one
+SELECT COUNT(*)
 FROM "chat"."conversation"
-WHERE "customer_id" = sqlc.arg('customer_id') AND "vendor_id" = sqlc.arg('vendor_id');
+WHERE (
+    ("id" = ANY(sqlc.slice('id')) OR sqlc.slice('id') IS NULL) AND
+    ("customer_id" = ANY(sqlc.slice('customer_id')) OR sqlc.slice('customer_id') IS NULL) AND
+    ("vendor_id" = ANY(sqlc.slice('vendor_id')) OR sqlc.slice('vendor_id') IS NULL) AND
+    ("last_message_at" = ANY(sqlc.slice('last_message_at')) OR sqlc.slice('last_message_at') IS NULL) AND
+    ("last_message_at" > sqlc.narg('last_message_at_from') OR sqlc.narg('last_message_at_from') IS NULL) AND
+    ("last_message_at" < sqlc.narg('last_message_at_to') OR sqlc.narg('last_message_at_to') IS NULL) AND
+    ("date_created" = ANY(sqlc.slice('date_created')) OR sqlc.slice('date_created') IS NULL) AND
+    ("date_created" > sqlc.narg('date_created_from') OR sqlc.narg('date_created_from') IS NULL) AND
+    ("date_created" < sqlc.narg('date_created_to') OR sqlc.narg('date_created_to') IS NULL)
+);
 
--- name: ListConversationByAccount :many
+-- name: ListChatConversation :many
 SELECT *
 FROM "chat"."conversation"
-WHERE "customer_id" = sqlc.arg('account_id') OR "vendor_id" = sqlc.arg('account_id')
-ORDER BY "last_message_at" DESC NULLS LAST
+WHERE (
+    ("id" = ANY(sqlc.slice('id')) OR sqlc.slice('id') IS NULL) AND
+    ("customer_id" = ANY(sqlc.slice('customer_id')) OR sqlc.slice('customer_id') IS NULL) AND
+    ("vendor_id" = ANY(sqlc.slice('vendor_id')) OR sqlc.slice('vendor_id') IS NULL) AND
+    ("last_message_at" = ANY(sqlc.slice('last_message_at')) OR sqlc.slice('last_message_at') IS NULL) AND
+    ("last_message_at" > sqlc.narg('last_message_at_from') OR sqlc.narg('last_message_at_from') IS NULL) AND
+    ("last_message_at" < sqlc.narg('last_message_at_to') OR sqlc.narg('last_message_at_to') IS NULL) AND
+    ("date_created" = ANY(sqlc.slice('date_created')) OR sqlc.slice('date_created') IS NULL) AND
+    ("date_created" > sqlc.narg('date_created_from') OR sqlc.narg('date_created_from') IS NULL) AND
+    ("date_created" < sqlc.narg('date_created_to') OR sqlc.narg('date_created_to') IS NULL)
+)
+ORDER BY "id"
 LIMIT sqlc.narg('limit')::int
 OFFSET sqlc.narg('offset')::int;
 
--- name: CountConversationByAccount :one
-SELECT COUNT(*)
-FROM "chat"."conversation"
-WHERE "customer_id" = sqlc.arg('account_id') OR "vendor_id" = sqlc.arg('account_id');
+-- name: ListCountChatConversation :many
+SELECT sqlc.embed(embed_conversation), COUNT(*) OVER() as total_count
+FROM "chat"."conversation" embed_conversation
+WHERE (
+    ("id" = ANY(sqlc.slice('id')) OR sqlc.slice('id') IS NULL) AND
+    ("customer_id" = ANY(sqlc.slice('customer_id')) OR sqlc.slice('customer_id') IS NULL) AND
+    ("vendor_id" = ANY(sqlc.slice('vendor_id')) OR sqlc.slice('vendor_id') IS NULL) AND
+    ("last_message_at" = ANY(sqlc.slice('last_message_at')) OR sqlc.slice('last_message_at') IS NULL) AND
+    ("last_message_at" > sqlc.narg('last_message_at_from') OR sqlc.narg('last_message_at_from') IS NULL) AND
+    ("last_message_at" < sqlc.narg('last_message_at_to') OR sqlc.narg('last_message_at_to') IS NULL) AND
+    ("date_created" = ANY(sqlc.slice('date_created')) OR sqlc.slice('date_created') IS NULL) AND
+    ("date_created" > sqlc.narg('date_created_from') OR sqlc.narg('date_created_from') IS NULL) AND
+    ("date_created" < sqlc.narg('date_created_to') OR sqlc.narg('date_created_to') IS NULL)
+)
+ORDER BY "id"
+LIMIT sqlc.narg('limit')::int
+OFFSET sqlc.narg('offset')::int;
 
--- name: CreateConversation :one
-INSERT INTO "chat"."conversation" ("customer_id", "vendor_id")
-VALUES ($1, $2)
+-- name: CreateChatConversation :one
+INSERT INTO "chat"."conversation" ("id", "customer_id", "vendor_id", "last_message_at", "date_created")
+VALUES ($1, $2, $3, $4, $5)
 RETURNING *;
 
--- name: UpdateConversationLastMessage :exec
+-- name: CreateBatchChatConversation :batchone
+INSERT INTO "chat"."conversation" ("id", "customer_id", "vendor_id", "last_message_at", "date_created")
+VALUES ($1, $2, $3, $4, $5)
+RETURNING *;
+
+-- name: CreateCopyChatConversation :copyfrom
+INSERT INTO "chat"."conversation" ("id", "customer_id", "vendor_id", "last_message_at", "date_created")
+VALUES ($1, $2, $3, $4, $5);
+
+-- name: CreateDefaultChatConversation :one
+INSERT INTO "chat"."conversation" ("customer_id", "vendor_id", "last_message_at")
+VALUES ($1, $2, $3)
+RETURNING *;
+
+-- name: CreateCopyDefaultChatConversation :copyfrom
+INSERT INTO "chat"."conversation" ("customer_id", "vendor_id", "last_message_at")
+VALUES ($1, $2, $3);
+
+-- name: UpdateChatConversation :one
 UPDATE "chat"."conversation"
-SET "last_message_at" = CURRENT_TIMESTAMP
-WHERE "id" = sqlc.arg('id');
+SET "customer_id" = COALESCE(sqlc.narg('customer_id'), "customer_id"),
+    "vendor_id" = COALESCE(sqlc.narg('vendor_id'), "vendor_id"),
+    "last_message_at" = CASE WHEN sqlc.arg('null_last_message_at')::bool = TRUE THEN NULL ELSE COALESCE(sqlc.narg('last_message_at'), "last_message_at") END,
+    "date_created" = COALESCE(sqlc.narg('date_created'), "date_created")
+WHERE id = sqlc.arg('id')
+RETURNING *;
+
+-- name: DeleteChatConversation :exec
+DELETE FROM "chat"."conversation"
+WHERE (
+    ("id" = ANY(sqlc.slice('id')) OR sqlc.slice('id') IS NULL) AND
+    ("customer_id" = ANY(sqlc.slice('customer_id')) OR sqlc.slice('customer_id') IS NULL) AND
+    ("vendor_id" = ANY(sqlc.slice('vendor_id')) OR sqlc.slice('vendor_id') IS NULL) AND
+    ("last_message_at" = ANY(sqlc.slice('last_message_at')) OR sqlc.slice('last_message_at') IS NULL) AND
+    ("last_message_at" > sqlc.narg('last_message_at_from') OR sqlc.narg('last_message_at_from') IS NULL) AND
+    ("last_message_at" < sqlc.narg('last_message_at_to') OR sqlc.narg('last_message_at_to') IS NULL) AND
+    ("date_created" = ANY(sqlc.slice('date_created')) OR sqlc.slice('date_created') IS NULL) AND
+    ("date_created" > sqlc.narg('date_created_from') OR sqlc.narg('date_created_from') IS NULL) AND
+    ("date_created" < sqlc.narg('date_created_to') OR sqlc.narg('date_created_to') IS NULL)
+);
--- internal/module/chat/db/queries/chat_message.sql
+++ internal/module/chat/db/queries/chat_message.sql
@@ -1,36 +1,108 @@
--- name: GetMessage :one
+-- name: GetChatMessage :one
 SELECT *
 FROM "chat"."message"
-WHERE "id" = sqlc.arg('id');
+WHERE ("id" = sqlc.narg('id'));
 
--- name: ListMessageByConversation :many
+-- name: CountChatMessage :one
+SELECT COUNT(*)
+FROM "chat"."message"
+WHERE (
+    ("id" = ANY(sqlc.slice('id')) OR sqlc.slice('id') IS NULL) AND
+    ("conversation_id" = ANY(sqlc.slice('conversation_id')) OR sqlc.slice('conversation_id') IS NULL) AND
+    ("sender_id" = ANY(sqlc.slice('sender_id')) OR sqlc.slice('sender_id') IS NULL) AND
+    ("type" = ANY(sqlc.slice('type')) OR sqlc.slice('type') IS NULL) AND
+    ("content" = ANY(sqlc.slice('content')) OR sqlc.slice('content') IS NULL) AND
+    ("status" = ANY(sqlc.slice('status')) OR sqlc.slice('status') IS NULL) AND
+    ("metadata" = ANY(sqlc.slice('metadata')) OR sqlc.slice('metadata') IS NULL) AND
+    ("date_created" = ANY(sqlc.slice('date_created')) OR sqlc.slice('date_created') IS NULL) AND
+    ("date_created" > sqlc.narg('date_created_from') OR sqlc.narg('date_created_from') IS NULL) AND
+    ("date_created" < sqlc.narg('date_created_to') OR sqlc.narg('date_created_to') IS NULL)
+);
+
+-- name: ListChatMessage :many
 SELECT *
 FROM "chat"."message"
-WHERE "conversation_id" = sqlc.arg('conversation_id')
-ORDER BY "date_created" DESC
+WHERE (
+    ("id" = ANY(sqlc.slice('id')) OR sqlc.slice('id') IS NULL) AND
+    ("conversation_id" = ANY(sqlc.slice('conversation_id')) OR sqlc.slice('conversation_id') IS NULL) AND
+    ("sender_id" = ANY(sqlc.slice('sender_id')) OR sqlc.slice('sender_id') IS NULL) AND
+    ("type" = ANY(sqlc.slice('type')) OR sqlc.slice('type') IS NULL) AND
+    ("content" = ANY(sqlc.slice('content')) OR sqlc.slice('content') IS NULL) AND
+    ("status" = ANY(sqlc.slice('status')) OR sqlc.slice('status') IS NULL) AND
+    ("metadata" = ANY(sqlc.slice('metadata')) OR sqlc.slice('metadata') IS NULL) AND
+    ("date_created" = ANY(sqlc.slice('date_created')) OR sqlc.slice('date_created') IS NULL) AND
+    ("date_created" > sqlc.narg('date_created_from') OR sqlc.narg('date_created_from') IS NULL) AND
+    ("date_created" < sqlc.narg('date_created_to') OR sqlc.narg('date_created_to') IS NULL)
+)
+ORDER BY "id"
 LIMIT sqlc.narg('limit')::int
 OFFSET sqlc.narg('offset')::int;
 
--- name: CountMessageByConversation :one
-SELECT COUNT(*)
-FROM "chat"."message"
-WHERE "conversation_id" = sqlc.arg('conversation_id');
+-- name: ListCountChatMessage :many
+SELECT sqlc.embed(embed_message), COUNT(*) OVER() as total_count
+FROM "chat"."message" embed_message
+WHERE (
+    ("id" = ANY(sqlc.slice('id')) OR sqlc.slice('id') IS NULL) AND
+    ("conversation_id" = ANY(sqlc.slice('conversation_id')) OR sqlc.slice('conversation_id') IS NULL) AND
+    ("sender_id" = ANY(sqlc.slice('sender_id')) OR sqlc.slice('sender_id') IS NULL) AND
+    ("type" = ANY(sqlc.slice('type')) OR sqlc.slice('type') IS NULL) AND
+    ("content" = ANY(sqlc.slice('content')) OR sqlc.slice('content') IS NULL) AND
+    ("status" = ANY(sqlc.slice('status')) OR sqlc.slice('status') IS NULL) AND
+    ("metadata" = ANY(sqlc.slice('metadata')) OR sqlc.slice('metadata') IS NULL) AND
+    ("date_created" = ANY(sqlc.slice('date_created')) OR sqlc.slice('date_created') IS NULL) AND
+    ("date_created" > sqlc.narg('date_created_from') OR sqlc.narg('date_created_from') IS NULL) AND
+    ("date_created" < sqlc.narg('date_created_to') OR sqlc.narg('date_created_to') IS NULL)
+)
+ORDER BY "id"
+LIMIT sqlc.narg('limit')::int
+OFFSET sqlc.narg('offset')::int;
 
--- name: CreateMessage :one
-INSERT INTO "chat"."message" ("conversation_id", "sender_id", "type", "content", "metadata")
-VALUES ($1, $2, $3, $4, $5)
+-- name: CreateChatMessage :one
+INSERT INTO "chat"."message" ("conversation_id", "sender_id", "type", "content", "status", "metadata", "date_created")
+VALUES ($1, $2, $3, $4, $5, $6, $7)
 RETURNING *;
 
--- name: MarkMessagesRead :exec
-UPDATE "chat"."message"
-SET "status" = 'Read'
-WHERE "conversation_id" = sqlc.arg('conversation_id')
-    AND "sender_id" != sqlc.arg('reader_id')
-    AND "status" != 'Read';
+-- name: CreateBatchChatMessage :batchone
+INSERT INTO "chat"."message" ("conversation_id", "sender_id", "type", "content", "status", "metadata", "date_created")
+VALUES ($1, $2, $3, $4, $5, $6, $7)
+RETURNING *;
 
--- name: CountUnreadMessages :one
-SELECT COUNT(*)
-FROM "chat"."message"
-WHERE "conversation_id" = sqlc.arg('conversation_id')
-    AND "sender_id" != sqlc.arg('reader_id')
-    AND "status" != 'Read';
+-- name: CreateCopyChatMessage :copyfrom
+INSERT INTO "chat"."message" ("conversation_id", "sender_id", "type", "content", "status", "metadata", "date_created")
+VALUES ($1, $2, $3, $4, $5, $6, $7);
+
+-- name: CreateDefaultChatMessage :one
+INSERT INTO "chat"."message" ("conversation_id", "sender_id", "content", "metadata")
+VALUES ($1, $2, $3, $4)
+RETURNING *;
+
+-- name: CreateCopyDefaultChatMessage :copyfrom
+INSERT INTO "chat"."message" ("conversation_id", "sender_id", "content", "metadata")
+VALUES ($1, $2, $3, $4);
+
+-- name: UpdateChatMessage :one
+UPDATE "chat"."message"
+SET "conversation_id" = COALESCE(sqlc.narg('conversation_id'), "conversation_id"),
+    "sender_id" = COALESCE(sqlc.narg('sender_id'), "sender_id"),
+    "type" = COALESCE(sqlc.narg('type'), "type"),
+    "content" = COALESCE(sqlc.narg('content'), "content"),
+    "status" = COALESCE(sqlc.narg('status'), "status"),
+    "metadata" = CASE WHEN sqlc.arg('null_metadata')::bool = TRUE THEN NULL ELSE COALESCE(sqlc.narg('metadata'), "metadata") END,
+    "date_created" = COALESCE(sqlc.narg('date_created'), "date_created")
+WHERE id = sqlc.arg('id')
+RETURNING *;
+
+-- name: DeleteChatMessage :exec
+DELETE FROM "chat"."message"
+WHERE (
+    ("id" = ANY(sqlc.slice('id')) OR sqlc.slice('id') IS NULL) AND
+    ("conversation_id" = ANY(sqlc.slice('conversation_id')) OR sqlc.slice('conversation_id') IS NULL) AND
+    ("sender_id" = ANY(sqlc.slice('sender_id')) OR sqlc.slice('sender_id') IS NULL) AND
+    ("type" = ANY(sqlc.slice('type')) OR sqlc.slice('type') IS NULL) AND
+    ("content" = ANY(sqlc.slice('content')) OR sqlc.slice('content') IS NULL) AND
+    ("status" = ANY(sqlc.slice('status')) OR sqlc.slice('status') IS NULL) AND
+    ("metadata" = ANY(sqlc.slice('metadata')) OR sqlc.slice('metadata') IS NULL) AND
+    ("date_created" = ANY(sqlc.slice('date_created')) OR sqlc.slice('date_created') IS NULL) AND
+    ("date_created" > sqlc.narg('date_created_from') OR sqlc.narg('date_created_from') IS NULL) AND
+    ("date_created" < sqlc.narg('date_created_to') OR sqlc.narg('date_created_to') IS NULL)
+);
