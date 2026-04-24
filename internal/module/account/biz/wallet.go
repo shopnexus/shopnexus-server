4e301eaa8033b7576940198c1c96d61410c56989
Khoa Bean <khoakomlem@gmail.com>
2026-04-24 11:29:39 +0700

    stub account wallet biz to unblock order refactor

--- internal/module/account/biz/wallet.go
+++ internal/module/account/biz/wallet.go
@@ -1,15 +1,8 @@
 package accountbiz
 
 import (
-	"errors"
-
 	"github.com/google/uuid"
-	"github.com/guregu/null/v6"
-	"github.com/jackc/pgx/v5"
 	restate "github.com/restatedev/sdk-go"
-
-	accountdb "shopnexus-server/internal/module/account/db/sqlc"
-	sharedmodel "shopnexus-server/internal/shared/model"
 )
 
 // WalletDebitParams holds the parameters for debiting a wallet.
@@ -35,150 +28,27 @@ type WalletCreditParams struct {
 	Note      string    `json:"note"`
 }
 
-// GetWalletBalance returns the wallet balance for the given account ID.
-// Returns 0 if no wallet exists yet.
-func (b *AccountHandler) GetWalletBalance(ctx restate.Context, accountID uuid.UUID) (int64, error) {
-	wallet, err := b.storage.Querier().GetWallet(ctx, accountdb.GetWalletParams{
-		AccountID: uuid.NullUUID{UUID: accountID, Valid: true},
-	})
-	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return 0, nil
-		}
-		return 0, sharedmodel.WrapErr("get wallet balance", err)
-	}
-	return wallet.Balance, nil
-}
-
 // WalletDebit deducts min(balance, amount) from the wallet, records a transaction,
 // and returns the amount actually deducted and the new balance.
 func (b *AccountHandler) WalletDebit(ctx restate.Context, params WalletDebitParams) (WalletDebitResult, error) {
-	var zero WalletDebitResult
-
-	type dbResult struct {
-		OldBalance int64 `json:"old_balance"`
-		NewBalance int64 `json:"new_balance"`
-	}
-
-	result, err := restate.Run(ctx, func(ctx restate.RunContext) (dbResult, error) {
-		// Get current balance to compute deducted amount
-		wallet, err := b.storage.Querier().GetWallet(ctx, accountdb.GetWalletParams{
-			AccountID: uuid.NullUUID{UUID: params.AccountID, Valid: true},
-		})
-		if err != nil {
-			if errors.Is(err, pgx.ErrNoRows) {
-				return dbResult{}, nil
-			}
-			return dbResult{}, err
-		}
-		oldBalance := wallet.Balance
-
-		updated, err := b.storage.Querier().DebitWallet(ctx, accountdb.DebitWalletParams{
-			AccountID: params.AccountID,
-			Amount:    params.Amount,
-		})
-		if err != nil {
-			return dbResult{}, err
-		}
-
-		_, err = b.storage.Querier().CreateWalletTransaction(ctx, accountdb.CreateWalletTransactionParams{
-			AccountID:   params.AccountID,
-			Type:        accountdb.AccountWalletTransactionTypePayment,
-			Amount:      oldBalance - updated.Balance,
-			ReferenceID: null.StringFromPtr(nullableString(params.Reference)),
-			Note:        null.StringFromPtr(nullableString(params.Note)),
-		})
-		if err != nil {
-			return dbResult{}, err
-		}
-
-		return dbResult{OldBalance: oldBalance, NewBalance: updated.Balance}, nil
-	})
-	if err != nil {
-		return zero, sharedmodel.WrapErr("wallet debit", err)
-	}
-
-	return WalletDebitResult{
-		Deducted: result.OldBalance - result.NewBalance,
-		Balance:  result.NewBalance,
-	}, nil
+	// TODO(account-refactor): re-implement against profile.balance + order.transaction ledger.
+	return WalletDebitResult{Deducted: 0, Balance: 0}, nil
 }
 
 // WalletCredit upserts the wallet and credits the given amount, recording a transaction.
 func (b *AccountHandler) WalletCredit(ctx restate.Context, params WalletCreditParams) error {
-	txType := accountdb.AccountWalletTransactionTypeRefund
-	switch params.Type {
-	case string(accountdb.AccountWalletTransactionTypePayment):
-		txType = accountdb.AccountWalletTransactionTypePayment
-	case string(accountdb.AccountWalletTransactionTypeTopUp):
-		txType = accountdb.AccountWalletTransactionTypeTopUp
-	}
-
-	if err := restate.RunVoid(ctx, func(ctx restate.RunContext) error {
-		_, err := b.storage.Querier().CreditWallet(ctx, accountdb.CreditWalletParams{
-			AccountID: params.AccountID,
-			Amount:    params.Amount,
-		})
-		if err != nil {
-			return err
-		}
-
-		_, err = b.storage.Querier().CreateWalletTransaction(ctx, accountdb.CreateWalletTransactionParams{
-			AccountID:   params.AccountID,
-			Type:        txType,
-			Amount:      params.Amount,
-			ReferenceID: null.StringFromPtr(nullableString(params.Reference)),
-			Note:        null.StringFromPtr(nullableString(params.Note)),
-		})
-		return err
-	}); err != nil {
-		return sharedmodel.WrapErr("wallet credit", err)
-	}
-
+	// TODO(account-refactor): re-implement against profile.balance + order.transaction ledger.
 	return nil
+}
+
+// GetWalletBalance returns the wallet balance for the given account ID.
+func (b *AccountHandler) GetWalletBalance(ctx restate.Context, accountID uuid.UUID) (int64, error) {
+	// TODO(account-refactor): re-implement against profile.balance.
+	return 0, nil
 }
 
 // ListWalletTransactions returns paginated wallet transactions for the given account.
 func (b *AccountHandler) ListWalletTransactions(ctx restate.Context, params ListWalletTransactionsParams) ([]WalletTransactionResult, error) {
-	txs, err := restate.Run(ctx, func(ctx restate.RunContext) ([]WalletTransactionResult, error) {
-		dbTxs, err := b.storage.Querier().ListWalletTransaction(ctx, accountdb.ListWalletTransactionParams{
-			AccountID: []uuid.UUID{params.AccountID},
-			Limit:     params.Limit,
-			Offset:    params.Offset(),
-		})
-		if err != nil {
-			return nil, err
-		}
-		results := make([]WalletTransactionResult, len(dbTxs))
-		for i, tx := range dbTxs {
-			var refID, note *string
-			if tx.ReferenceID.Valid {
-				refID = &tx.ReferenceID.String
-			}
-			if tx.Note.Valid {
-				note = &tx.Note.String
-			}
-			results[i] = WalletTransactionResult{
-				ID:          tx.ID,
-				AccountID:   tx.AccountID.String(),
-				Type:        string(tx.Type),
-				Amount:      tx.Amount,
-				ReferenceID: refID,
-				Note:        note,
-				DateCreated: tx.DateCreated.Format("2006-01-02T15:04:05Z07:00"),
-			}
-		}
-		return results, nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return txs, nil
-}
-
-func nullableString(s string) *string {
-	if s == "" {
-		return nil
-	}
-	return &s
+	// TODO(account-refactor): re-implement against order.transaction ledger.
+	return nil, nil
 }
